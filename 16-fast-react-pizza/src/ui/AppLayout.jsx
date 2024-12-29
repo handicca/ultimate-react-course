f96727db04e9744e7bbb8a3dfc9369d543e9d465
handicca <handikasyam.dev@gmail.com>
2024-12-29 09:57:54 +0700

    Completed Fast-React-Pizza project

---
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -0,0 +1,23 @@
+import { Outlet, useNavigation } from 'react-router';
+import CartOverview from '../features/cart/CartOverview';
+import Header from './Header';
+import Loader from './Loader';
+
+function AppLayout() {
+  const navigation = useNavigation();
+  const isLoading = navigation.state === 'loading';
+  return (
+    <div className="grid h-dvh grid-rows-[auto,1fr,auto] font-pizza">
+      {isLoading && <Loader />}
+      <Header />
+      <div className="overflow-auto">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
+      <CartOverview />
+    </div>
+  );
+}
+
+export default AppLayout;
---
+++ 16-fast-react-pizza/src/ui/Loader.jsx
@@ -0,0 +1,9 @@
+function Loader() {
+  return (
+    <div className="absolute z-50 inset-0 bg-stone-200/20 backdrop-blur-sm flex justify-center items-center">
+      <div className="loader"></div>
+    </div>
+  );
+}
+
+export default Loader;
